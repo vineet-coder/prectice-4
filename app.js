4b9dc8c41f6f1a6030964f7b4f25e801b2acc004
vineet <vineetsinghal2312@gmail.com>
2021-01-27 23:04:49 +0530

    fix

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var Btn1 = document.querySelector("#btn1");
 var Btn2 = document.querySelector("#btn2");
 var Input = document.querySelector("#input");
 var outputDiv = document.querySelector("#output");
+var sum = 0;
+var userInput = Input.value;
 function clickH1() {
-  var userInput = Input.value;
-  outputDiv.innerHTML = parseInt(userInput) + 1;
+  sum = parseInt(userInput) + 1;
+  outputDiv.innerHTML = sum;
 }
 function clickH2() {
-  var userInput = Input.value;
-  outputDiv.innerHTML = parseInt(userInput) - 1;
+  sum = userInput - 1;
+  outputDiv.innerHTML = sum;
 }
 
 Btn1.addEventListener("click", clickH1);
