4961e1cb22b980757c4db262db408f283fd42702
vineet <vineetsinghal2312@gmail.com>
2021-01-27 23:44:34 +0530

    fix

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ var Btn1 = document.querySelector("#btn1");
 var Btn2 = document.querySelector("#btn2");
 
 var outputDiv = document.querySelector("#output");
+var startingV = 0;
 function clickH1() {
-  var startingV = 0;
-
   var newV = startingV + 1;
   outputDiv.innerHTML = newV;
   newV = newV + 1;
 }
 function clickH2() {
-  var startingV = 0;
-
   var newV = startingV - 1;
   outputDiv.innerHTML = newV;
   newV = newV - 1;
